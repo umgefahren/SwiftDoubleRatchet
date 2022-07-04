7eec0fe787f0dd2aafaf94ac405c8b85aab0c35b
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-07-04 10:32:48 +0200

    more tests

--- Tests/SwiftDoubleRatchetTests/SwiftDoubleRatchetTests.swift
+++ Tests/SwiftDoubleRatchetTests/SwiftDoubleRatchetTests.swift
@@ -39,6 +39,49 @@ final class SwiftDoubleRatchetTests: XCTestCase {
         XCTAssertEqual(decrypted, message, "decrypted message and encrypted message are not equal")
     }
     
+    func testManyCurve25519() throws {
+        let aPrivate = Curve25519.KeyAgreement.Prv.init()
+        let bPrivate = Curve25519.KeyAgreement.Prv.init()
+        let shared = try aPrivate.aggreement(pub: bPrivate.publicKey)
+        var bobRatchet = try Ratchet<Curve25519.KeyAgreement>.init(sk: .init(data: shared))
+        var aliceRatchet = try Ratchet<Curve25519.KeyAgreement>.init(
+            sk: .init(data: shared), bobPublicKey: bobRatchet.sendingPublicKey)
+        for _ in 0...1000 {
+            let aliceMessage = Data.init(randomLength: 100)
+            let aliceAd = Data.init(randomLength: 10)
+            let (aliceHeader, aliceEncrypted) = try aliceRatchet.ratchetEncrypt(plaintext: aliceMessage, ad: aliceAd)
+            let aliceDecrypted = try bobRatchet.ratchetDecrypt(header: aliceHeader, ciphertext: aliceEncrypted, ad: aliceAd)
+            XCTAssertEqual(aliceDecrypted, aliceMessage, "decrypted message and encrypted message are not equal")
+            let bobMessage = Data.init(randomLength: 100)
+            let bobAd = Data.init(randomLength: 10)
+            let (bobHeader, bobEncrypted) = try bobRatchet.ratchetEncrypt(plaintext: bobMessage, ad: bobAd)
+            let bobDecrypted = try aliceRatchet.ratchetDecrypt(header: bobHeader, ciphertext: bobEncrypted, ad: bobAd)
+            XCTAssertEqual(bobDecrypted, bobMessage, "decrypted message and encrypted message are not equal")
+        }
+    }
+    
+    func testThrow() throws {
+        let aPrivate = Curve25519.KeyAgreement.Prv.init()
+        let bPrivate = Curve25519.KeyAgreement.Prv.init()
+        let shared = try aPrivate.aggreement(pub: bPrivate.publicKey)
+        var bobRatchet = try Ratchet<Curve25519.KeyAgreement>.init(sk: .init(data: shared))
+        var aliceRatchet = try Ratchet<Curve25519.KeyAgreement>.init(
+            sk: .init(data: shared), bobPublicKey: bobRatchet.sendingPublicKey)
+        let message = Data.init(randomLength: 100)
+        let ad = Data.init(randomLength: 10)
+        var header: Ratchet<Curve25519.KeyAgreement>.Header? = nil
+        var encrypted: Data? = nil
+        for _ in 0...11 {
+            (header, encrypted) = try aliceRatchet.ratchetEncrypt(plaintext: message, ad: ad)
+        }
+        
+        do {
+            let _ = try bobRatchet.ratchetDecrypt(header: header!, ciphertext: encrypted!, ad: ad)
+        } catch {
+            XCTAssertEqual(error as! Ratchet<Curve25519.KeyAgreement>.RatchetError, Ratchet<Curve25519.KeyAgreement>.RatchetError.skippedToMany, "didn't fail with to many skips")
+        }
+    }
+    
     func testBasicP256() throws {
         let aPrivate = P256.KeyAgreement.Prv.init()
         let bPrivate = P256.KeyAgreement.Prv.init()
